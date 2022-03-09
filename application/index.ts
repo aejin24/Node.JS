4d86f8176b1501785d51c82341ac0b2aca6b0d75
aejin24 <aejin24@naver.com>
2022-03-10 00:00:38 +0900

    delete dist folder & refactoring application

--- application/index.ts
+++ application/index.ts
@@ -1,17 +1,18 @@
-import express, { ErrorRequestHandler } from "express";
+import express, { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import { Main } from "@interface/render";
 import { StringOrNumber } from "@customTypes/union";
 
-dotenv.config({path: __dirname + "/../.env"});
+dotenv.config({path: __dirname + "\\.env"});
 
 const PORT: StringOrNumber = process.env.PORT || 3000;
 
 const app: express.Application = express();
-app.set("view engine", "pug");
-app.set("views", __dirname + "/../views");
 
-app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction): any => {
+app.set("view engine", "pug");
+app.set("views", __dirname + "\\views");
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.render("main", { _desc: "Hello Typescript" } as Main);
 });
 
