8de3ba94d27e52ab604e2d65cb6eb223fd054831
aejin24 <aejin24@naver.com>
2022-03-09 23:27:40 +0900

    modify types/express/index.d.ts

--- middleware/types/express/index.d.ts
+++ middleware/types/express/index.d.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { RequestTime } from "../custom/request";
+import { RequestTime } from "../../custom/request";
 
 // 새로 선언한 타입을 global로 설정한 경우 external 모듈이어야하기 때문에 export를 사용하여 에러가 안나도록 한다
 // 글로벌 객체를 확장하기 위해서 external, ambient 모듈이어야한다
